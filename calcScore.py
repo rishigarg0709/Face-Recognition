4d2a893f00ac30e8a1344a90a060c4d28c9b9338
rishi garg <rishigarg@rishis-MacBook-Pro.local>
2020-05-10 18:43:07 +0530

    minor changes

--- calcScore.py
+++ calcScore.py
@@ -7,15 +7,11 @@ from keras import backend as K
 def get_model_scores(faces):
     samples = np.array(faces, 'float32')
 
-    # prepare the data for the model
     samples = preprocess_input(samples, version=2)
 
-    # create a vggface model object
     model = VGGFace(model='resnet50',
       include_top=False,
       pooling='avg')
 
-    # perform prediction
-    
     return model.predict(samples.reshape(1,224,224,3))
 
