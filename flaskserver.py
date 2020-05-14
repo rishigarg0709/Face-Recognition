e96b36dd024f841fc8c4b0a057bc463b0b436d31
sid <siddharthagoel1998@gmail.com>
2020-05-14 11:20:03 +0530

    major improvements

--- flaskserver.py
+++ flaskserver.py
@@ -1,10 +1,36 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, jsonify
+from flask_cors import CORS
 from scipy.spatial.distance import cosine
 import extractFace
 import calcScore
 from keras import backend as K
+import pymongo
+from pymongo import MongoClient
+from flask_pymongo import PyMongo
+import urllib.request
+from bson.binary import Binary
+import pickle
+import json
+import base64
+
 
 app = Flask(__name__)
+CORS(app)
+cluster = MongoClient("mongodb+srv://rishi:mongo@cluster0-3sil9.mongodb.net/test?retryWrites=true&w=majority")
+app.config['MONGO_URI'] = "mongodb+srv://rishi:mongo@cluster0-3sil9.mongodb.net/test?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+
+db = cluster.minor
+testdb = cluster.test
+colUser = db.user
+cntr = db.counter
+chnk = testdb.fs.chunks
+
+def store_image(url, local_file_name):
+  with urllib.request.urlopen(url) as resource:
+    with open(local_file_name, 'wb') as f:
+      f.write(resource.read())
+
 
 @app.route('/')
 def enter():
@@ -12,42 +38,85 @@ def enter():
 
 mydict = {}
 
-@app.route('/' , methods = ['POST'])
+@app.route('/file/<filename>')
+def file(filename):
+    return mongo.send_file(filename)
+
+@app.route('/childInfo' , methods = ['POST'])
 def submit_pic():
     if request.method == 'POST':
-        f = request.files['childImage']
-        path = './static/{}'.format(f.filename)
-        f.save(path)
 
-        face = extractFace.extract_face_from_image(path)
+        name = request.form['name']
+        age = request.form['age']
+        city = request.form['city']
+        gname = request.form['gname']
+        email = request.form['email']
+        f = request.files['childImage']
+
+        image_string = base64.b64encode(f.read())
+        image_string = image_string.decode('utf-8')
+        
+        curid = colUser.count_documents({}) + 1
+
+        path = './static/' 
+        imgURL = ""
+        imgURL = "data:image/jpeg;base64," + image_string
+        store_image(imgURL,path + f.filename)
+
+        face = extractFace.extract_face_from_image(path + f.filename)
         score = calcScore.get_model_scores(face)
 
         ismatched = False
-        imgname = ""
-        imgpath = ""
+        curChildData = {"name" : name,
+                        "age" : age,
+                        "city" : city,
+                        "gname" : gname, 
+                        "email" : email,
+                        "imgURL" : imgURL,
+                        "db_img": f.filename,
+                        "score" : Binary(pickle.dumps(score, protocol=2), subtype=128 ),
+                        "isFound" : False}
+        foundChildData = {}
 
-        if(len(mydict) == 0):
-            mydict[f.filename] = score
+        
+        if(curid==1):
+            colUser.insert_one(curChildData)
         else:
-            for img,scr in mydict.items():
-                if cosine(scr,score) <= 0.4:
+            for data in colUser.find():
+                scoreArr = pickle.loads(data['score'])
+                if cosine(score, scoreArr) <= 0.4:
                     ismatched = True
-                    imgname = img
+                    foundChildData = data
                     break
 
         if(ismatched):
-            imgpath = './static/{}'.format(imgname)
-        else:
-            mydict[f.filename] = score
+            curChildData["isFound"] = True
+            colUser.insert_one(curChildData)
+            colUser.update_one({"imgURL" : foundChildData["imgURL"]},
+                               {"$set" : {"isFound" : True}})
+        elif curid !=1 and ismatched == False:
+            colUser.insert_one(curChildData)
 
-        result_dic = {
-            'path' : path,
-            'imgpath' : imgpath,
-            'ismatched' : ismatched
-        }
+        del foundChildData['_id']
+        del foundChildData['score']
+        del curChildData['_id']
+        del curChildData['score']
 
-    K.clear_session()
-    return render_template('index.html', result = result_dic)
+        K.clear_session()
+        return jsonify({"curChild":curChildData, "foundChild": foundChildData})
 
-if __name__ == "__main__":
-    app.run(debug=True) 
+
+@app.route('/lost', methods = ['GET'])
+def lost():
+    return render_template('lost.html')
+
+
+@app.route('/found', methods = ['GET'])
+def found():
+    return render_template('found.html')
+
+
+# if __name__ == "__main__":
+app.run(threaded = False) 
+
+# cd ~/Face-Recognition && python flaskserver.py
